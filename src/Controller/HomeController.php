40b83dd574549807f0619db205c88de459705d32
Jihed Mannai <jihed.mannai@eprit.tn.com>
2023-03-05 02:54:05 +0100

    crud+template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,11 +2,29 @@
 
 namespace App\Controller;
 
-use App\Entity\Tournois;
-use App\Repository\TournoisRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Exploit;
+use App\Entity\User;
+use App\Form\SignupformType;
+use App\Repository\UserRepository;
+use App\Security\EmailVerifier;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+//use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+//use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\VarDumper\VarDumper;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class HomeController extends AbstractController
 {
@@ -50,30 +68,62 @@ class HomeController extends AbstractController
         ]);
     }
 
-  
-
-    #[Route('/tournoisfront', name: 'app_tournois', methods: ['GET'])]
-    public function index5(TournoisRepository $tournoisRepository): Response
+    #[Route('/tournois', name: 'app_tournois')]
+    public function index5(): Response
     {
         return $this->render('home/tournois.html.twig', [
-            'tournois' => $tournoisRepository->findAll(),
-        ]);
-    }
-
-
-    #[Route('/tournoisdetails/{id}', name: 'app_details', methods: ['GET'])]
-    public function show(Tournois $tournoi): Response
-    {
-        return $this->render('home/tournoisdetails.html.twig', [
-            'tournoi' => $tournoi,
-        ]);
-    }
-
-    #[Route('/login', name: 'app_login')]
-    public function index6(): Response
-    {
-        return $this->render('home/login.html.twig', [
             'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/signup', name: 'app_signup')]
+    public function index6(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+
+        $user = new User();
+
+        $form = $this->createForm(SignupformType::class,$user);
+
+        $form->handleRequest($request);
+
+
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            // encode the plain password
+            $userData = $form->getData();
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $userData->getPassword()
+                )
+               
+            );
+            /** @var UploadedFile $uploadedFile */
+            $uploadedFile=$form['image']->getData();
+            $destination = $this->getParameter('kernel.project_dir').'/public/uploads';
+            $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+            $newFile = $originalFilename.'-'.uniqid().'.'.$uploadedFile->guessExtension();
+            $uploadedFile->move(
+                $destination,
+                $newFile
+               );
+
+               $user->setImage($newFile);
+
+               $user->setRoles(['ROLE_USER']);
+
+               $entityManager->persist($user);
+               $entityManager->flush();
+
+        }
+
+
+        
+        return $this->render('home/register.html.twig', [
+            'controller_name' => 'HomeController',
+            'formUser' => $form->createView(),
+            "user" => $user ,
         ]);
     }
 
@@ -90,6 +140,26 @@ class HomeController extends AbstractController
     {
         return $this->render('home/panier.html.twig', [
             'controller_name' => 'HomeController',
+            
+        ]);
+    }
+    
+    
+    #[Route('/login', name: 'app_login')]
+    public function index9(authenticationUtils $authenticationUtils,Request $request ,EntityManagerInterface $entityManager): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $email = $authenticationUtils->getLastUsername();
+       
+       
+        
+        return $this->render('security/login.html.twig', [
+            'email' => $email , 
+            'error' => $error ,
+            
         ]);
     }
 }
