f8d7d32df16c09e15f0edd13bf754d65e24d9a28
eyayassmine <80338919+eyayassmine@users.noreply.github.com>
2023-03-06 12:59:05 +0100

    Add files via upload

--- src/Entity/Reclamation.php
+++ src/Entity/Reclamation.php
@@ -2,41 +2,198 @@
 
 namespace App\Entity;
 
-use App\Repository\ReclamationRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
-#[ORM\Entity(repositoryClass: ReclamationRepository::class)]
+/**
+ * Reclamation
+ *
+ * @ORM\Table(name="reclamation")
+ * @ORM\Entity
+ * @Vich\Uploadable
+ */
 class Reclamation
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="idReclamation", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idreclamation;
 
-    #[ORM\Column(length: 255)]
-    private ?string $description = null;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255, nullable=false)
+     */
+    private $nom;
 
-    #[ORM\ManyToOne(inversedBy: 'reclamations')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?user $_user = null;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string", length=255, nullable=false)
+     */
+    private $prenom;
 
-    #[ORM\OneToMany(mappedBy: 'description', targetEntity: Reponse::class)]
-    private Collection $reponses;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, nullable=false)
+     */
+    private $email;
 
-    #[ORM\Column(type: Types::DATE_MUTABLE)]
-    private ?\DateTimeInterface $createdAt = null;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="screenshot", type="string", length=255, nullable=false)
+     */
+    private $screenshot;
 
-    public function __construct()
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="numero_mobile", type="string", length=255, nullable=false)
+     */
+    private $numeroMobile;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_creation", type="date", nullable=false)
+     */
+    private $dateCreation;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="date_traitement", type="date", nullable=true)
+     */
+    private $dateTraitement;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255, nullable=false)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="status", type="string", length=255, nullable=false)
+     */
+    private $status;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nomServcie", type="string", length=255, nullable=false)
+     */
+    private $nomservcie;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $image;
+
+    /**
+     * @Vich\UploadableField(mapping="products", fileNameProperty="image")
+     * @var File
+     */
+    private $imageFile;
+
+    public function getIdreclamation(): ?int
     {
-        $this->reponses = new ArrayCollection();
+        return $this->idreclamation;
     }
 
-    public function getId(): ?int
+    public function getNom(): ?string
     {
-        return $this->id;
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getScreenshot(): ?string
+    {
+        return $this->screenshot;
+    }
+
+    public function setScreenshot(string $screenshot): self
+    {
+        $this->screenshot = $screenshot;
+
+        return $this;
+    }
+
+    public function getNumeroMobile(): ?string
+    {
+        return $this->numeroMobile;
+    }
+
+    public function setNumeroMobile(string $numeroMobile): self
+    {
+        $this->numeroMobile = $numeroMobile;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->dateCreation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $dateCreation): self
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    public function getDateTraitement(): ?\DateTimeInterface
+    {
+        return $this->dateTraitement;
+    }
+
+    public function setDateTraitement(?\DateTimeInterface $dateTraitement): self
+    {
+        $this->dateTraitement = $dateTraitement;
+
+        return $this;
     }
 
     public function getDescription(): ?string
@@ -51,57 +208,54 @@ class Reclamation
         return $this;
     }
 
-    public function getUser(): ?user
+    public function getStatus(): ?string
     {
-        return $this->_user;
+        return $this->status;
     }
 
-    public function setUser(?user $_user): self
+    public function setStatus(string $status): self
     {
-        $this->_user = $_user;
+        $this->status = $status;
 
         return $this;
     }
 
-    /**
-     * @return Collection<int, Reponse>
-     */
-    public function getReponses(): Collection
+    public function getNomservcie(): ?string
     {
-        return $this->reponses;
+        return $this->nomservcie;
     }
 
-    public function addReponse(Reponse $reponse): self
+    public function setNomservcie(string $nomservcie): self
     {
-        if (!$this->reponses->contains($reponse)) {
-            $this->reponses->add($reponse);
-            $reponse->setDescription($this);
+        $this->nomservcie = $nomservcie;
+
+        return $this;
+    }
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
         }
-
-        return $this;
     }
 
-    public function removeReponse(Reponse $reponse): self
+    public function getImageFile()
     {
-        if ($this->reponses->removeElement($reponse)) {
-            // set the owning side to null (unless already changed)
-            if ($reponse->getDescription() === $this) {
-                $reponse->setDescription(null);
-            }
-        }
-
-        return $this;
+        return $this->imageFile;
     }
 
-    public function getCreatedAt(): ?\DateTimeInterface
+    public function setImage($image)
     {
-        return $this->createdAt;
+        $this->image = $image;
     }
 
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    public function getImage()
     {
-        $this->createdAt = $createdAt;
-
-        return $this;
+        return $this->image;
     }
 }
